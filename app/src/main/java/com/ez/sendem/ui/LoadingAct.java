7ba3be037cdfc0e3d86df6edd16f5db74666fcbc
susantizhang <susanti.zhang@gmail.com>
2017-02-21 21:26:29 +0700

    pengecekan read contact
    penambahan pengecekan read contact untuk api versi 23 dan setelahnya

--- app/src/main/java/com/ez/sendem/ui/LoadingAct.java
+++ app/src/main/java/com/ez/sendem/ui/LoadingAct.java
@@ -1,6 +1,10 @@
 package com.ez.sendem.ui;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.ez.sendem.R;
 import com.ez.sendem.manager.PageManager;
@@ -9,6 +13,8 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class LoadingAct extends RootAct {
+
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,34 @@ public class LoadingAct extends RootAct {
     @Override
     public void onResume(){
         super.onResume();
+        checkPermission();
+    }
+
+    private void checkPermission() {
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+        } else {
+            // Android version is lesser than 6.0 or the permission is already granted.
+            openMainApp();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted
+                openMainApp();
+            } else {
+                Toast.makeText(this, "Until you grant the permission, we cannot display the names", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void openMainApp(){
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
